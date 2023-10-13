914d2d7a442c40df958429d88a5526a614385d3a
Leo Picado <leonardo.picado@zenjob.com>
2023-10-14 01:14:43 +0200

    WIP: filtering is working, needs to be cleaned up

--- src/index.js
+++ src/index.js
@@ -19,12 +19,42 @@ function d(s) {
   return document.querySelector(s);
 }
 
+function render(line) {
+  const checkedLines = localStorage.getItem("lines");
+  const isChecked = checkedLines.split(",").includes(line) ? "checked" : "";
+
+  return `<li style="display: inline-block; margin-right: 20px;">
+            <input type="checkbox" id="line-${line}" data-line="${line}" name="lines" style="height: 24px; width: 24px;" ${isChecked}/>  
+            <label for="line-${line}">${line}</label>
+          </li>`;
+}
+
+function filterDepartures(departures) {
+  let listOfLines = [... new Set(departures.map(ride => ride.line.productName).sort())];
+  lines.innerHTML = listOfLines.map(line => render(line)).join("");
+  const lineList = Array.from(document.getElementsByName("lines"));
+  const selectedLines = lineList.filter(e => e.checked);
+  const checkedLines = selectedLines.map(e => e.dataset.line);
+  return departures.filter(ride => checkedLines.includes(ride.line.productName));
+}
+
+function storeFilters() {
+  const lineList = Array.from(document.getElementsByName("lines"));
+  lineList.forEach(el => el.addEventListener('change', function(chk) {
+      const selectedLines = lineList.filter(e => e.checked);
+      const checkedLines = selectedLines.map(e => e.dataset.line);
+      window.localStorage.setItem("lines", checkedLines);
+      init();
+  }));
+}
+
 async function init() {
   const depTitle = d("#dep-title");
   const depContainer = d("#dep-container");
   const lastUpdated = d("#last-updated");
   const footer = d("#footer");
   const status = d("#status");
+  const lines = d("#lines");
   const ctx = { formatTime, formatIcon, formatWaitTime };
   const stopSearch = document.location.hash || "Storkower";
 
@@ -33,7 +63,9 @@ async function init() {
     const { departures, realtimeDataUpdatedAt } = await getDepartures(stop.id);
     depTitle.innerText = stop.name;
     document.title = stop.name + " Departures";
-    depContainer.innerHTML = departures.map(dep => parse(DEPARTURE_TMPL, dep, ctx)).join("");
+    const filteredDepartures = filterDepartures(departures);
+    storeFilters();
+    depContainer.innerHTML = filteredDepartures.map(dep => parse(DEPARTURE_TMPL, dep, ctx)).join("");
     lastUpdated.innerText = formatDate(new Date().getTime());
     footer.style.visibility = "visible";
     setTimeout(() => refresh(stop), REFRESH_INTERVAL);
@@ -44,6 +76,7 @@ async function init() {
     status.innerText = "Stop not found!";
     status.className = "error";
   }
+
   refresh(stop);
 }
 
